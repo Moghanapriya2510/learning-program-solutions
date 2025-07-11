41ccf105f4e9539224d33047a122e49dec30d2b8
Moghana priya <147739803+Moghanapriya2510@users.noreply.github.com>
2025-07-11 16:52:56 +0530

    Add files via upload

---
+++ Week4/Spring REST using Spring Boot 3/2. spring-rest-handson/Hello World RESTful Web Service/Code/HelloWorldServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.cognizant.hello_world_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
